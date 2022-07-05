4e65fa45c6f70aa66e3316174f7f47f36385f49e
Lucas Costa e Silva Almeida <lucas.almeida@compasso.com.br>
2022-07-05 19:07:10 -0300

    Mudanças no layout da página de sorteio

--- src/components/lista/index.tsx
+++ src/components/lista/index.tsx
@@ -5,11 +5,13 @@ import listStyle from './Lista.module.scss'
 export default function ListaParticipantes(){
   const participantes: string[] = useListaParticipantes();
   return(
-    <>
-      <img
-        src={sacolas}
-        className={listStyle.image}
-      />
+    <div style={{display: 'flex'}}>
+      <div className={listStyle.image1}>
+        <img
+          src={sacolas}
+        />
+      </div>
+      <div>
       <ul className={listStyle.lista}>
         {participantes.map( 
           participante => 
@@ -21,7 +23,13 @@ export default function ListaParticipantes(){
           )
         }
       </ul>
-    </>
+      </div>
+      <div className={listStyle.image2}>
+        <img
+          src={sacolas}
+        />
+      </div>
+    </div>
 
   )
 }
